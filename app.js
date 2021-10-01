1b80a81578cddd5cc2b397126bcd5c80c8474d86
Joel <joelmwood@live.com>
2021-10-01 07:21:49 -0500

    Add html/ejs pages

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+app.use(express.static("public"));
+app.set('view-engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://admin-joel:Test123@cluster0.x17z2.mongodb.net/usersDB", {
+  useNewUrlParser: true
+});
+
+const userSchema = ({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get
+
+
+app.listen(3000, function(){
+    console.log("Server Started Successfully");
+})
