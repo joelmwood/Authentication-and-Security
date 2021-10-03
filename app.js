b34e2cc300fda1d54ba8f19485b5b9083a79ab68
Joel <joelmwood@live.com>
2021-10-03 06:57:42 -0500

    Move secret from app.js to .env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -7,24 +8,71 @@ const encrypt = require("mongoose-encryption");
 
 const app = express();
 
+console.log(process.env.API_KEY);
+
 app.use(express.static("public"));
-app.set('view-engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb+srv://admin-joel:Test123@cluster0.x17z2.mongodb.net/usersDB", {
   useNewUrlParser: true
 });
 
-const userSchema = ({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(encrypt,  {secret: process.env.SECRET, encryptedFields: ["password"] });
+
 const User = new mongoose.model("User", userSchema);
 
-app.get
+app.route("/")
+    .get(function(req, res){    
+        res.render("home");    
+    })
+;
 
+app.route("/register")
+    .get(function(req, res){    
+        res.render("register");    
+    })
+    .post(function(req, res){
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save(function(err){
+            if (!err){
+                res.render("secrets");
+            } else {
+                console.log(err);
+            }
+        });    
+    })
+;
+app.route("/login")
+    .get(function(req, res){    
+        res.render("login");    
+    })
+    .post(function(req, res){
+        const username = req.body.username;
+        const password = req.body.password;
+        User.findOne({email: username}, function(err, foundUser){
+            if(err){
+                console.log(err);
+            } else {
+                if (foundUser){
+                    if (foundUser.password === password){
+                        res.render("secrets");
+                    }
+                }
+            }
+        });
+    });
+    
+;
 
 app.listen(3000, function(){
     console.log("Server Started Successfully");
-})
+});
