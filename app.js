037764009add3b8bf36215cc1c068117ba767200
Joel <joelmwood@live.com>
2021-10-03 07:20:05 -0500

    Upgrade from encryption to hashing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -23,7 +24,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt,  {secret: process.env.SECRET, encryptedFields: ["password"] });
+//userSchema.plugin(encrypt,  {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +41,7 @@ app.route("/register")
     .post(function(req, res){
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
         newUser.save(function(err){
             if (!err){
@@ -57,7 +58,7 @@ app.route("/login")
     })
     .post(function(req, res){
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
         User.findOne({email: username}, function(err, foundUser){
             if(err){
                 console.log(err);
